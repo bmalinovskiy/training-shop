6dfae44cb2e74f110b35c17e209b17565651146f
Bogdan Malinovskiy <bamalinovskij@gmail.com>
2022-02-15 08:30:36 +0300

    feat: add slider to main page

--- src/components/main-blocks/top-section/banners/banners.jsx
+++ src/components/main-blocks/top-section/banners/banners.jsx
@@ -2,16 +2,25 @@ import React from 'react';
 
 import { Link } from 'react-router-dom';
 
+import switchLeft from '../../../../images/product/switch-left.svg';
+import switchRight from '../../../../images/product/switch-right.svg';
+
 import styles from './banners.module.scss';
 
 const Banners = () => {
   return (
     <div className={styles.banners}>
       <div className={styles.slider}>
+        <button type='button' className={styles.viewSwitcher}>
+          <img src={switchLeft} alt='Switch left' />
+        </button>
         <div className={styles.sliderBanner}>
           <span className={styles.bannerLabel}>BANNER</span>
           <span className={styles.sliderTitle}>YOUR TITLE TEXT</span>
         </div>
+        <button type='button' className={styles.viewSwitcher}>
+          <img src={switchRight} alt='Switch right' />
+        </button>
       </div>
       <div className={styles.womenBanner}>
         <Link to='/women'>
