96fbec0394930377c837b16eb32419c7d450f325
Bogdan Malinovskiy <bamalinovskij@gmail.com>
2022-03-22 15:50:08 +0300

    fix: delete unused routes

--- src/constants/routes.js
+++ src/constants/routes.js
@@ -1,13 +1,8 @@
 const ROUTES = {
   root: '/',
   product: '/:category/:id',
-  aboutUs: '/about',
   women: '/women',
   men: '/men',
-  beauty: '/beauty',
-  accessories: '/accessories',
-  blog: '/blog',
-  contact: '/contact',
 };
 
 export default ROUTES;
