e4aa72c41b5df06c9b8822ac83bd127203fcde45
Bogdan Malinovskiy <bmalinovskiyy@gmail.com>
2022-03-25 13:58:25 +0300

    fix: filter code refactoring

--- src/components/filter/filter.jsx
+++ src/components/filter/filter.jsx
@@ -32,22 +32,40 @@ const Filter = ({ productType }) => {
   );
   const priceList = useMemo(() => ['$500+', '$200-500', '$100-200', '$50-100', '$0-50'], []);
 
-  const handleChange = useCallback(
-    ({ target: { value } }) => {
-      if (colorList.includes(value)) {
-        dispatch(changeColorFilter({ value }));
-      }
-      if (sizeList.includes(value)) {
-        dispatch(changeSizeFilter({ value }));
-      }
-      if (brandList.includes(value)) {
-        dispatch(changeBrandFilter({ value }));
-      }
-      if (priceList.includes(value)) {
-        dispatch(changePriceFilter({ value }));
+  const filterList = [
+    { name: 'color', items: colorList },
+    { name: 'size', items: sizeList },
+    { name: 'brand', items: brandList },
+    { name: 'price', items: priceList },
+  ];
+
+  const itemsFoundList = [
+    { name: 'color', items: colorFilters },
+    { name: 'size', items: sizeFilters },
+    { name: 'brand', items: brandFilters },
+    { name: 'price', items: priceFilters },
+  ];
+
+  const handleChangeFilter = useCallback(
+    (name, value) => {
+      switch (name) {
+        case 'color':
+          dispatch(changeColorFilter({ value }));
+          break;
+        case 'size':
+          dispatch(changeSizeFilter({ value }));
+          break;
+        case 'brand':
+          dispatch(changeBrandFilter({ value }));
+          break;
+        case 'price':
+          dispatch(changePriceFilter({ value }));
+          break;
+        default:
+          break;
       }
     },
-    [dispatch, brandList, colorList, priceList, sizeList]
+    [dispatch]
   );
 
   useEffect(() => {
@@ -57,74 +75,34 @@ const Filter = ({ productType }) => {
   return (
     <>
       <div className={styles.filterList} data-test-id={`filters-${productType}`}>
-        <div className={styles.item}>
-          <span className={styles.title}>COLOR</span>
-          <div className={styles.list} data-test-id='filters-color'>
-            {colorList.map((color) => (
-              <div key={color}>
-                <input type='checkbox' value={color} onChange={handleChange} data-test-id={`filter-color-${color}`} />
-                <label htmlFor={color}>{color}</label>
-              </div>
-            ))}
+        {filterList.map(({ name, items }) => (
+          <div className={styles.item} key={name}>
+            <span className={styles.title}>{name.toUpperCase()}</span>
+            <div className={styles.list}>
+              {items.map((item) => (
+                <div key={item}>
+                  <input
+                    type='checkbox'
+                    value={item}
+                    onChange={({ target: { value } }) => handleChangeFilter(name, value)}
+                  />
+                  <label htmlFor={item}>{item}</label>
+                </div>
+              ))}
+            </div>
           </div>
-        </div>
-        <div className={styles.item}>
-          <span className={styles.title}>SIZE</span>
-          <div className={styles.list} data-test-id='filters-size'>
-            {sizeList.map((size) => (
-              <div key={size}>
-                <input type='checkbox' value={size} onChange={handleChange} data-test-id={`filter-size-${size}`} />
-                <label htmlFor={size}>{size}</label>
-              </div>
-            ))}
-          </div>
-        </div>
-        <div className={styles.item}>
-          <span className={styles.title}>BRAND</span>
-          <div className={styles.list} data-test-id='filters-brand'>
-            {brandList.map((brand) => (
-              <div key={brand}>
-                <input type='checkbox' value={brand} onChange={handleChange} data-test-id={`filter-brand-${brand}`} />
-                <label htmlFor={brand}>{brand}</label>
-              </div>
-            ))}
-          </div>
-        </div>
-        <div className={styles.item}>
-          <span className={styles.title}>PRICE</span>
-          <div className={styles.list}>
-            {priceList.map((price) => (
-              <div key={price}>
-                <input type='checkbox' value={price} onChange={handleChange} />
-                <label htmlFor={price}>{price}</label>
-              </div>
-            ))}
-          </div>
-        </div>
+        ))}
       </div>
       {itemsFound !== null && (
-        <div className={styles.appliedFilters}>
-          <span className={styles.itemsFound}>{`${itemsFound} items found`}</span>
-          <span>
-            {colorFilters.map((color) => (
-              <span key={color} className={styles.item}>{`Color: ${color}`}</span>
-            ))}
-          </span>
-          <span>
-            {sizeFilters.map((size) => (
-              <span key={size} className={styles.item}>{`Size: ${size}`}</span>
-            ))}
-          </span>
-          <span>
-            {brandFilters.map((brand) => (
-              <span key={brand} className={styles.item}>{`Brand: ${brand}`}</span>
-            ))}
-          </span>
-          <span>
-            {priceFilters.map((price) => (
-              <span key={price} className={styles.item}>{`Price: ${price}`}</span>
-            ))}
-          </span>
+        <div className={styles.itemsFoundList}>
+          <span className={styles.title}>{`${itemsFound} items found`}</span>
+          {itemsFoundList.map(({ name, items }) => (
+            <span key={name}>
+              {items.map((item) => (
+                <span key={item} className={styles.item}>{`${name}: ${item}`}</span>
+              ))}
+            </span>
+          ))}
         </div>
       )}
     </>
