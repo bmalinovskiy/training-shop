31998087841580c9df1f55b705dec63c20afae73
Bogdan Malinovskiy <bmalinovskiyy@gmail.com>
2022-04-15 10:38:02 +0300

    refactor: change payment methods to contains Cash

--- src/constants/shopping-cart.js
+++ src/constants/shopping-cart.js
@@ -1,7 +1,6 @@
 import payPalIcon from '../images/shopping-cart/pay-pal.svg';
 import visaIcon from '../images/shopping-cart/visa.svg';
 import mastercardIcon from '../images/shopping-cart/mastercard.svg';
-import cashIcon from '../images/shopping-cart/cash.svg';
 
 export const ORDER_SUCCESS = 'success';
 
@@ -15,5 +14,4 @@ export const PAYMENT_METHODS = [
   { id: '1', name: 'PayPal', imgPath: payPalIcon },
   { id: '2', name: 'Visa', imgPath: visaIcon },
   { id: '3', name: 'Mastercard', imgPath: mastercardIcon },
-  { id: '4', name: 'Cash', imgPath: cashIcon },
 ];
